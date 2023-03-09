6742d02744ed6e3376ecfdf025c926da9a013dce
printer130 <leonardotobe94@gmail.com>
2023-03-09 00:14:18 -0400

    readm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import preact from '@astrojs/preact';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+/* import sitemap from '@astrojs/sitemap' */
+import preact from '@astrojs/preact'
+import tailwind from '@astrojs/tailwind'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), preact(), tailwind()],
-  output: 'server',
-  adapter: vercel()
-});
+	site: 'https://example.com',
+	integrations: [mdx(), preact(), tailwind()],
+	output: 'server',
+	adapter: vercel()
+})
