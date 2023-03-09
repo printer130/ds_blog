91016b1ca50291cb5ea8b3d4c70d5cd3420edd56
printer130 <leonardotobe94@gmail.com>
2023-03-09 00:35:31 -0400

    assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-/* import sitemap from '@astrojs/sitemap' */
+import sitemap from '@astrojs/sitemap'
 import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
 
@@ -8,8 +8,8 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), preact(), tailwind()],
+	site: 'https://ds-blog-sigma.vercel.app',
+	integrations: [mdx(), preact(), tailwind(), sitemap()],
 	output: 'server',
 	adapter: vercel()
 })
