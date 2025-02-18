fbad4ad0937abe2e4756178c665e879d070e698d
printer <leonardotobe94@gmail.com>
2025-02-17 23:22:52 -0400

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,19 @@ import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/static'
 
-const url = import.meta.VERCEL_ENV === 'production' ? 'https://asurbanipal.xyz' : 'http://localhost:3001'
+const url =
+	import.meta.VERCEL_ENV === 'production'
+		? 'https://asurbanipal.xyz'
+		: 'http://localhost:3001'
 
 export default defineConfig({
 	integrations: [mdx(), preact(), tailwind(), sitemap()],
 	output: 'static',
 	adapter: vercel(),
 	site: url,
+	image: {
+		remotePatterns: [{ protocol: 'https' }]
+	},
 	experimental: {
 		assets: true,
 		prerender: true
